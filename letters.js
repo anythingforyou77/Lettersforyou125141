6462a610b850aa504072011f6cdc17b0f016b386
anythingforyou77 <youngandumb.io@gmail.com>
2026-07-26 11:06:46 +0530

    Update letters.js

--- letters.js
+++ letters.js
@@ -1,15 +1,31 @@
 const letters = [
 
 `
-Dear you,
+My dearest C,
 
-Welcome to the very first letter.
+Innu thott nte trip start cheyyanallo,
+so i thought if enik nte kutty kk proper ayy
+message ayakaan pattillengi athin compensate cheyyaan
+ndelum onn vende. Athond njaan decide cheythatha
+12 days 12 letters for my gorgeous girl😌❤️.
 
-Some moments are too special to rush,
-so I wanted to leave these little pieces
-of my heart here, one day at a time.
+Inn njan full train ll avum, aellarum oppam erikal avumbo
+enik voice ayakaan patillelum text cheyyyan nokandutto..
+Eni eppo No phone nn ulla rule okke kond varo avoo🧑🏻‍🦯
+Ravile ayachallum elleum.. Night njammak call cheyyaam
+motham story um alkare pattiyum njan paranj tharaam. Insha allah
+and enik ante avde ulla kathakalum paranj tharane 
+kedakunnathinu mumb...
+Trip nte edell annelu orikalum eyy enik oru disturbance 
+avillaa ttoodaa.. So ninte manasilum anganathe thoughts
+kond varallee.. Class erikumbo okkee enik msg ayachoo..
+pattunna pole reply tharaam.. 
 
-I hope this letter brings a smile ❤️
+Ith oru introduction pole kanda mathiyee😅 njaan erunn next
+11 letters ndhaa aezuthande enik areela😂
+
+
+With Love, Your chekkaan❤️
 `,
 
 
